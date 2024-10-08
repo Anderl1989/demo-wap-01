e84645e3358d4ed4d53ce0560e875a3951908138
Andreas Kasch <andreas.kasch@fh-hagenberg.at>
2024-10-08 11:20:49 +0200

    feature x

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import b from './b.js';
 import c from './c.js';
+import x from './x.js';
 
 function a() {
-    console.log('This is A');
+    console.log('Hello world');
 }
 
 a();
 b();
 c();
+x();
